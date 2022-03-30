aaac175c047f42675fa3eb88f2d4ed45ebdf004b
dani <photomassacre@gmail.com>
2022-03-30 11:47:59 -0700

    Updating the scaffolding and also configuing the thoughts

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts2', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
